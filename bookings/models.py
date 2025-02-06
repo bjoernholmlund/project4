1717b4623f2fddce7ae285aa7ff9ea2875359c0f
bjoernholmlund <bjoern.holmlund@gmail.com>
2025-02-06 08:02:34 +0000

    create models for my bookings, tables, and users

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
+
+
+    def __str__(self):
+        return f"table {self.table_number}"
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    number_of_guests = models.IntegerField()
+
+    def __str__(self):
+        return f"Booking for {self.user} on {self.date_time}"
+    
+class Menu(models.Model):
+    name = models.CharField(max_length=215)
+    description = models.TextField()
+    pårice = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-92pbymz%*6=!cp-u#03k!0k88-!sk%z#-)$6o3($%v52^u+=%z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "bookings",
 ]
 
 MIDDLEWARE = [
