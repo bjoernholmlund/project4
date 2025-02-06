7a4d7944b45bc4b362f3b078736dc01a2a8e358c
bjoernholmlund <bjoern.holmlund@gmail.com>
2025-02-06 09:19:48 +0000

    creating a view in bookings/views.py

--- bookings/views.py
+++ bookings/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Booking, Table
+from .forms import BookingForm
+from django.contrib import messages
+from django.utils.timezone import now
 
 
-# Create your views here.
+# What does this view do?
+# If a POST request is submitted, we create a booking.
+# We check if the table is already booked at the selected time.
+# If it is available, the booking is saved, otherwise the user receives an error message.
+
+def book_table(request):
+   if request.method == 'Post':
+      form = BookingForm(request.Post)
+      if form.is_valid():
+            booking_date = form.cleaned_data['data_time']
+            table = form.cleaned_data['table']
 
 
-def my_bookings(request):
-   return HttpResponse("Hello my restaurantfriend!")
+            # Check if the table is available on that date
+      existing_booking = Booking.objects.filter(table=table, date_time=booking_date).exists()
+
+      if existing_booking:
+           messages.error(request, "The table is already booked at this time. Choose another time or another table.")        
+        
+      else:
+         form.save()
+         messages.success(request, "Your booking is confirmed!")
+         return redirect('book_table') # Send back to the booking page or a confirmation page
+        
+   else:
+       form = bookingForm()
+
+   return render(request, 'boookings/book_table.html', {'form': form})    
+
+
 
