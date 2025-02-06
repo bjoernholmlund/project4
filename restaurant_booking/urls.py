f0cc90de71438c9a9d941efd00aee179e72eb736
bjoernholmlund <bjoern.holmlund@gmail.com>
2025-02-06 13:23:14 +0000

    Create registration and login forms

--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('bookings/', include('bookings.urls')), # Link to the booking app
 ]
