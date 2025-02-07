ce4e0149f9ef551877d5bc09d30c2548116735ff
bjoernholmlund <bjoern.holmlund@gmail.com>
2025-02-07 10:27:50 +0000

    trying to fix allowed_host

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,11 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-92pbymz%*6=!cp-u#03k!0k88-!sk%z#-)$6o3($%v52^u+=%z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS =['127.0.0.1',
-                'localed host',
-                'my-booking-table-project-8aa24a46de03.herokuapp.com']
+ALLOWED_HOSTS =['127.0.0.1', 'my-booking-table-project-8aa24a46de03.herokuapp.com']
 
 # Application definition
 
